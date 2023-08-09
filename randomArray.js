40dc0cdfcd978cbff37c9369a3425b3673cf8ca0
MeiyaRam <MeiyaRam@gmail.com>
2023-08-09 13:53:17 +0530

    created a 2D Array with random length and fill a to inner arrays.

---
+++ randomArray.js
@@ -0,0 +1,13 @@
+const { map, range } = require('@laufire/utils/collection');
+const { rndBetween } = require('@laufire/utils/lib');
+const arrayCount = 10;
+
+const getTwoDArray = () =>
+  map(range(1, arrayCount), () =>
+    map(range(0, rndBetween(1, 10)), () => 'a'));
+
+
+const main = () =>
+  console.log(getTwoDArray());
+  
+main();
